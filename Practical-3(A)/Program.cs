a00979a1d522e4fe7ec1d8ce5927905e80243e48
Jay Shah <jayshah06162@gmail.com>
2022-02-02 21:45:40 +0530

    Initial Commit

---
+++ Practical-3(A)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Program
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int num, deno;
+
+            Console.WriteLine("Enter Numerator : ");
+            //   num = int.Parse(Console.ReadLine());
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Denominator : ");
+            deno = Convert.ToInt32(Console.ReadLine());
+
+            int quotient = num / deno;
+            int remainder = num % deno;
+
+            Console.WriteLine($"Integer division result : {quotient} with a remainder : {remainder}");
+
+            double frac_quotient = Math.Round((double)num / deno);
+
+            Console.WriteLine($"Fractional Quotient : {frac_quotient}");
+            Console.WriteLine($"Floating Point Division Result : {(float)num/deno}");
+            Console.WriteLine($"Mixed Fraction : {quotient} {remainder}/{deno}");
+        }
+    }
+}
+
---
+++ Practical-3(B)/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Program
+{
+
+    class ExString
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("============== FINDING THE LENGTH OF A STRING ==============");
+            string s;
+            Console.WriteLine($"Enter a String : ");
+            s = Console.ReadLine();
+
+            Console.WriteLine($"The length of the String is : {s.Length}\n");
+
+
+
+
+            Console.WriteLine("============== Checking The Type of The String ==============");
+            Console.WriteLine($"Enter a String : ");
+            s = Console.ReadLine();
+
+            int len = s.Length;
+
+            if ((char)s[len - 1] == '.')
+            {
+                Console.WriteLine($"String \"{s}\" is Declarative Sentence");
+            }
+            else if ((char)s[len - 1] == '!')
+            {
+                Console.WriteLine($"String \"{s}\" is Exclamation Sentence\n");
+            }
+            else if ((char)s[len - 1] == '?')
+            {
+                Console.WriteLine($"String \"{s}\" is Interrogatory Sentence");
+            }
+            else
+            {
+                Console.WriteLine($"String \"{s}\" is not a  Sentence");
+            }
+
+            Console.WriteLine("============== Enter First Name And Last Name Seperated By Space ==============");
+            Console.WriteLine("Enter your full name : ");
+            string ss = Console.ReadLine();
+            int size = ss.Length;
+            int index = ss.IndexOf(" ");
+
+
+            if (index != -1)
+            {
+                // Console.WriteLine($"{index}");
+                // Console.WriteLine($"{len}");
+
+                string firstName = ss.Substring(0, index);
+                string lastName = ss.Substring(index + 1, size);
+                Console.WriteLine($"{firstName} , {lastName}");
+            }
+            else
+                Console.WriteLine($"{ss}");
+        }
+    }
+}
